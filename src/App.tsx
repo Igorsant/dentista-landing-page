8725aa6f0143cadb2596eef1471bfd2a79644d22
Igor Oliveira dos Santos <igor.santos@hp.com>
2025-06-29 18:12:55 -0300

    change text colors

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Perfil from "/perfil.png"
 
 export default function App() {
   return (
-    <div className="font-sans text-gray-800">
+    <div className="font-sans text-amber-900">
       {/* Hero section */}
         <section className="overflow-hidden">
           <div className="mx-auto max-w-7xl pt-10 px-6 lg:flex lg:px-8 lg:items-end">
@@ -17,11 +17,11 @@ export default function App() {
                 Dra. Luciana Plutarco
               </h1>
               <p className="mt-8 text-lg font-medium text-pretty text-amber-900 sm:text-xl/8">
-                Com mais de 30 anos de experiência, a Dra. Luciana Plutarco é uma referência em odontologia estética e saúde da mulher. Sua clínica oferece tratamentos personalizados para atender às necessidades de cada paciente, sempre com foco na segurança e bem-estar.
+                Com mais de 30 anos de experiência, a Dra. Luciana Plutarco é uma referência em odontologia estética e saúde. Sua clínica oferece tratamentos personalizados para atender às necessidades de cada paciente, sempre com foco na segurança e bem-estar.
               </p>
               <div className="mt-10 flex items-center gap-x-6">
                 <a
-                  href="#"
+                  href="https://wa.me/message/5ZDAZSCW6IOMC1"
                   className="rounded-md bg-teal-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-teal-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
                 >
                   Agende sua consulta
@@ -40,7 +40,7 @@ export default function App() {
 
       {/* Serviços */}
       <section className="bg-gray-100 py-16 px-6">
-        <h2 className="text-2xl font-semibold text-center mb-8">Serviços</h2>
+        <h2 className="text-2xl font-semibold text-center mb-8 text-amber-950">Serviços</h2>
         <div className="grid md:grid-cols-3 gap-6 max-w-5xl mx-auto">
           {[
             "Limpeza e prevenção",
@@ -62,18 +62,18 @@ export default function App() {
 
       {/* Contato */}
       <section id="contato" className="py-16 px-6 max-w-4xl mx-auto">
-        <h2 className="text-2xl font-semibold mb-6">Contato</h2>
+        <h2 className="text-2xl font-semibold mb-6 text-amber-950">Contato</h2>
         <p>
           <strong>WhatsApp:</strong>{" "}
           <a href="https://wa.me/5599999999999" className="text-teal-600">
-            +55 99 99999-9999
+            +55 85 98576-7080
           </a>
         </p>
         <p>
-          <strong>Endereço:</strong> Av. Principal, 123 - Fortaleza/CE
+          <strong>Endereço:</strong> Rua Carolina Sucupira, 345 - Sala 16 - Fortaleza/CE
         </p>
         <p>
-          <strong>Email:</strong> contato@draana.com.br
+          <strong>Email:</strong> lpnmlb@hotmail.com
         </p>
       </section>
 
